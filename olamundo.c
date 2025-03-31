8c508d65a4448f76155428a66e4a9210de651862
Pardaldz7 <igordasilvacarvalho.carvalho@gmail.com>
2025-03-31 20:13:02 +0000

    treinamento

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main (){
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+
+    printf("Entre com a temperatura: \n");
+    scanf("%f",&temperatura);
+    printf("Entre com a umidade: \n");
+    scanf("%f", &umidade);
+    printf("Entre com estoque:? \n");
+    scanf("%u", &estoque);
+
+
+    if (temperatura >30){
+        printf("temperatura está alta\n");
+    } else{
+        printf("Temperatura está dentro dos parâmetros\n");
+
+
+    if (umidade >50){
+        printf("umidade elevada\n");
+    } else{
+        printf("Umidades está dentro dos parâmetros\n");
+    }
+
+    if (estoque > estoqueMinimo){
+        printf ("Estoque abaixo do minimo!\n");
+    } else {
+        printf("Estoque normal!\n");
+    }
+
+    }
+
+}
